9620e793b7e18ffb0d0e11ae78e79930af7b0d5d
TrackLab <tracklab@tracklab.dev>
2021-05-21 15:51:10 +0200

    Character follows headset
    Added some Scripts to make the character follow the headset, their feet and whole body. Since addition tracker do not run yet.

---
+++ Assets/Scripts/HomegrownScripts/FollowHeadSetWithOffset.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHeadSetWithOffset : MonoBehaviour
+{
+
+    public Transform CopyFrom;
+    public Transform PasteTo;
+    public bool CopyRotation = false;
+
+    [Header("Positional Offsets")]
+    public bool ApplyXOffset = false;
+    public float XOffset = 0f;
+    public bool OverwriteYPosition = false;
+    public float YPos = 0f;
+    public bool ApplyZOffset = false;
+    public float ZOffset = 0f;
+
+    [Header("Rotational Offsets")]
+    public bool ApplyAngles = false;
+    public float XAngle = 0f;
+    public float YAngle = 0f;
+    public float ZAngle = 0f;
+
+    private float Xpos, Ypos, Zpos;
+    private Quaternion rotate;
+    private Vector3 position;
+
+    
+    //public bool OverwriteHeightForChest = false;
+    
+    void Update()
+    {
+        if (ApplyXOffset) {Xpos = CopyFrom.position.x+XOffset;}
+        else {Xpos = CopyFrom.position.x;}
+
+        if (OverwriteYPosition) {Ypos = YPos;}
+        else {Ypos = CopyFrom.position.y;}
+
+        if (ApplyZOffset) {Zpos = CopyFrom.position.z+ZOffset;}
+        else {Zpos = CopyFrom.position.z;}
+        
+        position = new Vector3(Xpos,Ypos,Zpos);
+
+        if (CopyRotation){rotate = new Quaternion(CopyFrom.rotation.x,CopyFrom.rotation.y,CopyFrom.rotation.z,CopyFrom.rotation.w);} 
+        else {rotate = new Quaternion(PasteTo.rotation.x,PasteTo.rotation.y,PasteTo.rotation.z,PasteTo.rotation.w);}
+        
+        if(ApplyAngles){rotate*=Quaternion.Euler(XAngle,YAngle,ZAngle);}
+
+        PasteTo.SetPositionAndRotation(position,rotate);
+    }
+}
