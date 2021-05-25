89497ae45532306acc8933a877136aa4f8dcc1f8
TrackLab <tracklab@tracklab.dev>
2021-05-25 16:25:52 +0200

    Several additions and changes
    A few unused Assets have been removed, a new Logo has been added to serve as Program Logo, added the ViRe logo in the screen corner,

---
+++ Assets/Scripts/HomegrownScripts/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public new Transform transform;
+    public float XRotate = 0;
+    public float YRotate = 0;
+    public float ZRotate = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(XRotate,YRotate,ZRotate);
+    }
+}
