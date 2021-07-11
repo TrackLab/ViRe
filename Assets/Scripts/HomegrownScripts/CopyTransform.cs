f9a6f32bfbb52e03cebe607be943e5fe9616ca8a
SarenDev <54124554+SarenDev@users.noreply.github.com>
2021-07-11 18:33:30 +0200

    refactor: Consolidate all Transform-related scripts

---
+++ Assets/Scripts/HomegrownScripts/CopyTransform.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+
+    public Transform CopyFrom;
+    private Transform PasteTo;
+    public bool EnableCopyLocation = true;
+    public bool EnableCopyRotation = false;
+
+    [Header("Position Copy Parameters")]
+    public bool ApplyXOffset = false;
+    public bool LockX = false;
+    public float X = 0f;
+    public bool ApplyYOffset = false;
+    public bool LockY = false;
+    public float Y = 0f;
+    public bool ApplyZOffset = false;
+    public bool LockZ = false;
+    public float Z = 0f;
+
+    [Header("Rotation Copy Parameters")]
+    public bool ApplyXAngleOffset = false;
+    public bool LockXAngle = false;
+    public float XAngle = 0f;
+    public bool ApplyYAngleOffset = false;
+    public bool LockYAngle = false;
+    public float YAngle = 0f;
+    public bool ApplyZAngleOffset = false;
+    public bool LockZAngle = false;
+    public float ZAngle = 0f;
+
+    private float Xpos, Ypos, Zpos, Xquat, Yquat, Zquat;
+    private Quaternion rotate = new Quaternion();
+    private Vector3 position;
+
+    void Start(){
+        paramCheck();
+        PasteTo = GetComponent<Transform>();
+    }
+    
+    void Update()
+    {
+        if (EnableCopyLocation){
+            if (ApplyXOffset) {Xpos = CopyFrom.position.x-X;}
+            else if (LockX) {Xpos = X;}
+            else {Xpos = CopyFrom.position.x;}
+
+            if (ApplyYOffset) {Ypos = CopyFrom.position.y-Y;}
+            else if (LockY) {Ypos = Y;}
+            else {Ypos = CopyFrom.position.y;}
+
+            if (ApplyZOffset) {Zpos = CopyFrom.position.z-Z;}
+            else if (LockZ) {Zpos = Z;}
+            else{Zpos = CopyFrom.position.z;}
+        } else {
+            Xpos = PasteTo.position.x;
+            Ypos = PasteTo.position.y;
+            Zpos = PasteTo.position.z;
+        }
+        
+        position = new Vector3(Xpos,Ypos,Zpos);
+
+
+        if (EnableCopyRotation){
+            if (ApplyXAngleOffset){Xquat = CopyFrom.eulerAngles.x-XAngle;}
+            else if (LockXAngle){Xquat = XAngle;}
+            else {Xquat = CopyFrom.eulerAngles.x;}
+
+            if (ApplyYAngleOffset){Yquat = CopyFrom.eulerAngles.y-YAngle;}
+            else if (LockYAngle){Yquat = YAngle;}
+            else {Yquat = CopyFrom.eulerAngles.y;}
+
+            if (ApplyZAngleOffset){Zquat = CopyFrom.eulerAngles.z-ZAngle;}
+            else if (LockZAngle){Zquat = ZAngle;}
+            else {Zquat = CopyFrom.eulerAngles.z;}
+
+
+            rotate = Quaternion.Euler(Xquat,Yquat,Zquat);
+        } 
+        else {rotate = new Quaternion(CopyFrom.rotation.x,CopyFrom.rotation.y,CopyFrom.rotation.z,CopyFrom.rotation.w);}
+
+        PasteTo.SetPositionAndRotation(position,rotate);
+    }
+
+    void paramCheck(){
+        if (ApplyXOffset && LockX){
+            Debug.LogWarning("Both Lock X and Apply X Offset are set, only one can be used! Lock X is ignored.");
+            LockX = false;
+        }
+        if (ApplyYOffset && LockY){
+            Debug.LogWarning("Both Lock Y and Apply Y Offset are set, only one can be used! Lock Y is ignored.");
+            LockY = false;
+        }
+        if (ApplyZOffset && LockZ){
+            Debug.LogWarning("Both Lock Z and Apply Z Offset are set, only one can be used! Lock Z is ignored.");
+            LockZ = false;
+        }
+        if (ApplyXAngleOffset && LockXAngle){
+            Debug.LogWarning("Both Lock X Angle and Apply X Angle Offset are set, only one can be used! Lock X Angle is ignored.");
+            LockXAngle = false;
+        }
+        if (ApplyYAngleOffset && LockYAngle){
+            Debug.LogWarning("Both Lock Y Angle and Apply Y Angle Offset are set, only one can be used! Lock Y Angle is ignored.");
+            LockYAngle = false;
+        }
+        if (ApplyZAngleOffset && LockZAngle){
+            Debug.LogWarning("Both Lock Z Angle and Apply Z Angle Offset are set, only one can be used! Lock Z Angle is ignored.");
+            LockZAngle = false;
+        }
+    }
+}
---
+++ Assets/Scripts/HomegrownScripts/SpinObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpinObject : MonoBehaviour
+{
+    private Transform trans;
+    public float XRotate = 0;
+    public float YRotate = 0;
+    public float ZRotate = 0;
+
+    void Start(){
+        trans = GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.Rotate(XRotate,YRotate,ZRotate);
+    }
+}
