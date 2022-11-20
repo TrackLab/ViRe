55f4cc2c4b31035b148128238ea60e8f3e49bf7e
SarenDev <committer@saren.dev>
2022-11-20 14:30:34 +0100

    refactor(IK): Replace FastIK with Unity Rigging

---
+++ Assets/Scripts/RigController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/**
+    Pulled from https://github.com/immersive-insiders/ReadyPlayerMe_BodyTracking/blob/main/Assets/Scripts/AvatarController.cs
+**/
+
+[System.Serializable]
+public class MapTransform
+{
+    public Transform vrTarget;
+    public Transform IKTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void MapVRAvatar()
+    {
+        IKTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        IKTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+public class RigController : MonoBehaviour
+{
+    [SerializeField] private MapTransform head;
+    [SerializeField] private MapTransform leftHand;
+    [SerializeField] private MapTransform rightHand;
+
+    [SerializeField] private float turnSmoothness;
+
+    [SerializeField] private Transform IKHead;
+
+    [SerializeField] private Vector3 headBodyOffset;
+
+    void LateUpdate()
+    {
+        transform.position = IKHead.position + headBodyOffset;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(IKHead.forward, Vector3.up).normalized, Time.deltaTime * turnSmoothness); ;
+        head.MapVRAvatar();
+        leftHand.MapVRAvatar();
+        rightHand.MapVRAvatar();
+    }
+}
