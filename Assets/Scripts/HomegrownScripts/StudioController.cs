99b0959e6fb4b1900a62ee793f1399aed7607921
SarenDev <committer@saren.dev>
2022-11-30 20:06:44 +0100

    fix: Load framerate and IK prefs on boot

--- Assets/Scripts/HomegrownScripts/StudioController.cs
+++ Assets/Scripts/HomegrownScripts/StudioController.cs
@@ -6,6 +6,7 @@ public class StudioController : MonoBehaviour
 {
     private FeedbackColor feedlight;
     private BVHRecorder recorder;
+    private IKFootSolver leftLeg, rightLeg;
     public GameObject pauseScreen;
     
 
@@ -18,11 +19,18 @@ public class StudioController : MonoBehaviour
         GameObject light = GameObject.Find("AirText");
         feedlight = light.GetComponent<FeedbackColor>();
         recorder = GetComponent<BVHRecorder>();
+        leftLeg = GameObject.Find("LeftAnkleController").GetComponent<IKFootSolver>();
+        rightLeg = GameObject.Find("RightAnkleController").GetComponent<IKFootSolver>();
 
         string docPath = System.Environment.GetEnvironmentVariable("USERPROFILE")+"/Documents";
         string recPath = PlayerPrefs.GetString("RecPath", docPath);
         if (!Directory.Exists(recPath)){recPath = docPath;}
         recorder.directory = recPath;
+        recorder.frameRate = PlayerPrefs.GetInt("RecFPS", 60);
+
+
+        leftLeg.stepDistance = rightLeg.stepDistance = PlayerPrefs.GetFloat("IKthreshold", leftLeg.stepDistance);
+        leftLeg.stepLength = rightLeg.stepLength = PlayerPrefs.GetFloat("IKdistance", leftLeg.stepLength);
 
         UpdateCheck updateCheck = GetComponent<UpdateCheck>();
         updateCheck.check();
