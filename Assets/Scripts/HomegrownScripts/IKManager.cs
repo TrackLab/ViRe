6ef07913503ade0c0476ab4c98fd2ef4dd3e41ce
SarenDev <committer@saren.dev>
2024-10-21 00:51:13 +0200

    refactor(IK): Use Transform Overrides for IK tracker override

--- Assets/Scripts/HomegrownScripts/IKManager.cs
+++ Assets/Scripts/HomegrownScripts/IKManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 using Valve.VR;
@@ -9,58 +7,40 @@ public class IKManager : MonoBehaviour
     [SerializeField] GameObject rootPoseTracker;
     [SerializeField] GameObject midPoseTracker;
     [SerializeField] GameObject tipPoseTracker;
-    [SerializeField] Transform rootBone;
-    [SerializeField] Transform midBone;
-    [SerializeField] Transform tipBone;
+
+    [SerializeField] GameObject rootOverride;
+    [SerializeField] GameObject midOverride;
+    [SerializeField] GameObject tipOverride;
 
     [SerializeField] RigBuilder rigBuilder;
 
-    // This is a hack to allow for a split IK on a limb
-    private ChainIKConstraint defaultLimbIK;
-    // private TwoBoneIKConstraint secondaryIK;
     private SteamVR_Behaviour_Pose rootPoseNode;
     private SteamVR_Behaviour_Pose midPoseNode;
     private SteamVR_Behaviour_Pose tipPoseNode;
-    // private Transform rootPoseTransform;
-    // private Transform midPoseTransform;
-    private Transform tipPoseTransform;
-    private Transform defaultTipTransform;
 
     private void ConfigureIK()
     {
-        //FIXME: Restoring doesn't work
-        defaultLimbIK.data.root = rootBone;
-        defaultLimbIK.data.tip = tipBone;
-        defaultLimbIK.data.target = defaultTipTransform;
-        defaultLimbIK.enabled = true;
-        // secondaryIK.enabled = false;
+        // Turn everything off to (maybe) reset the positions
+        gameObject.SetActive(false);
+        rootOverride.SetActive(false);
+        rigBuilder.Build();
 
-        if (rootPoseNode.isValid && midPoseNode.isValid && tipPoseNode.isValid)
-        {
-            defaultLimbIK.enabled = false;
-            // secondaryIK.enabled = false;
-            // Might need to rebuild
-            return;
-        }
-
-        //FIXME: Freezes various IK elements
         if (rootPoseNode.isValid)
         {
-            defaultLimbIK.data.root = midBone;
+            rootOverride.SetActive(true);
         }
 
-        // if (midPoseNode.isValid)
-        // {
-        //     defaultLimbIK.data.root = tipBone;
-        //     defaultLimbIK.data.tip = tipBone;
-        //     secondaryIK.enabled = true;
+        if (midPoseNode.isValid)
+        {
+            midOverride.SetActive(true);
+        }
 
-        // }
+        if (tipPoseNode.isValid)
+        {
+            tipOverride.SetActive(true);
+        }
 
-        // if (tipPoseNode.isValid)
-        // {
-        //     defaultLimbIK.data.target = tipPoseTransform;
-        // }
+        gameObject.SetActive(true);
 
         rigBuilder.Build();
     }
@@ -70,21 +50,11 @@ public class IKManager : MonoBehaviour
         ConfigureIK();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        defaultLimbIK = GetComponent<ChainIKConstraint>();
-        // secondaryIK = GetComponent<TwoBoneIKConstraint>();
-
         rootPoseNode = rootPoseTracker.GetComponent<SteamVR_Behaviour_Pose>();
         midPoseNode = midPoseTracker.GetComponent<SteamVR_Behaviour_Pose>();
         tipPoseNode = tipPoseTracker.GetComponent<SteamVR_Behaviour_Pose>();
-
-        // rootPoseTransform = rootPoseTracker.GetComponent<Transform>();
-        // midPoseTransform = midPoseTracker.GetComponent<Transform>();
-        tipPoseTransform = tipPoseTracker.GetComponent<Transform>();
-
-        defaultTipTransform = defaultLimbIK.data.target;
 
         ConfigureIK();
     }
