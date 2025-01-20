dbe7e3b87dbe61b11d6dc645aee0b6020167b6da
SarenDev <committer@saren.dev>
2025-01-20 16:06:04 +0100

    refactor(scripts): resolve optimization suggestions

--- Assets/Scripts/HomegrownScripts/FlyCamera.cs
+++ Assets/Scripts/HomegrownScripts/FlyCamera.cs
@@ -1,34 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
- 
-public class FlyCamera : MonoBehaviour {
- 
-    private float mainSpeed = 5.0f; //regular speed
-    private float camSens = 0.25f; //How sensitive it with mouse
+
+public class FlyCamera : MonoBehaviour
+{
+
+    private readonly float mainSpeed = 5.0f; //regular speed
+    private readonly float camSens = 0.25f; //How sensitive it with mouse
     private Vector2 p;
     private Vector3 lastMouse;
     private bool activateCam = false;
 
     public GameObject DroneObject;
 
-    public void onMove(InputAction.CallbackContext value){
+    public void onMove(InputAction.CallbackContext value)
+    {
         p = -value.ReadValue<Vector2>();
-        p = p * mainSpeed * Time.deltaTime;
+        p = mainSpeed * Time.deltaTime * p;
     }
 
     // TODO: Camera flips when the X angle is either max or min. Needs to be clipped
-    public void onMouse(InputAction.CallbackContext value){
+    public void onMouse(InputAction.CallbackContext value)
+    {
         lastMouse = value.ReadValue<Vector2>();
         lastMouse = new Vector2(lastMouse.y * camSens, lastMouse.x * camSens);
         lastMouse = new Vector2(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y);
     }
 
-    public void onCameraActivate(InputAction.CallbackContext value){
+    public void onCameraActivate(InputAction.CallbackContext value)
+    {
         activateCam = !value.canceled;
     }
 
-    void LateUpdate(){
-        DroneObject.transform.Translate(new Vector3(p.x,0,p.y));
-        if (activateCam){transform.eulerAngles = lastMouse;}
+    void LateUpdate()
+    {
+        DroneObject.transform.Translate(new Vector3(p.x, 0, p.y));
+        if (activateCam) { transform.eulerAngles = lastMouse; }
     }
 }
--- Assets/Scripts/HomegrownScripts/SteamVRLaserWrapper.cs
+++ Assets/Scripts/HomegrownScripts/SteamVRLaserWrapper.cs
@@ -1,57 +1,62 @@
- using UnityEngine;
- using UnityEngine.EventSystems;
- using Valve.VR.Extras;
- 
- public class SteamVRLaserWrapper : MonoBehaviour
- {
-     /**
-        This script lets the SteamVR laser interact with Unity Buttons using Box Colliders
-        Offered up by a gracious soul on the Unity Forums
-        TODO: Find original author
-     **/
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Valve.VR.Extras;
 
-     private SteamVR_LaserPointer steamVrLaserPointer;
- 
-     private void Awake()
-     {
-         steamVrLaserPointer = gameObject.GetComponent<SteamVR_LaserPointer>();
-         steamVrLaserPointer.PointerIn += OnPointerIn;
-         steamVrLaserPointer.PointerOut += OnPointerOut;
-         steamVrLaserPointer.PointerClick += OnPointerClick;
-     }
- 
-     private void OnPointerClick(object sender, PointerEventArgs e)
-     {
-         Debug.Log(e.target.name);
-         IPointerClickHandler clickHandler = e.target.GetComponent<IPointerClickHandler>();
-         if (clickHandler == null)
-         {
-             return;
-         }
- 
- 
-         clickHandler.OnPointerClick(new PointerEventData(EventSystem.current));
-     }
- 
-     private void OnPointerOut(object sender, PointerEventArgs e)
-     {
-         IPointerExitHandler pointerExitHandler = e.target.GetComponent<IPointerExitHandler>();
-         if (pointerExitHandler == null)
-         {
-             return;
-         }
- 
-         pointerExitHandler.OnPointerExit(new PointerEventData(EventSystem.current));
-     }
- 
-     private void OnPointerIn(object sender, PointerEventArgs e)
-     {
-         IPointerEnterHandler pointerEnterHandler = e.target.GetComponent<IPointerEnterHandler>();
-         if (pointerEnterHandler == null)
-         {
-             return;
-         }
- 
-         pointerEnterHandler.OnPointerEnter(new PointerEventData(EventSystem.current));
-     }
- }
+public class SteamVRLaserWrapper : MonoBehaviour
+{
+    /**
+       This script lets the SteamVR laser interact with Unity Buttons using Box Colliders
+       Offered up by a gracious soul on the Unity Forums
+       TODO: Find original author
+    **/
+
+    private SteamVR_LaserPointer steamVrLaserPointer;
+
+    private void Awake()
+    {
+        steamVrLaserPointer = gameObject.GetComponent<SteamVR_LaserPointer>();
+        steamVrLaserPointer.PointerIn += OnPointerIn;
+        steamVrLaserPointer.PointerOut += OnPointerOut;
+        steamVrLaserPointer.PointerClick += OnPointerClick;
+    }
+
+    private void OnPointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.TryGetComponent<IPointerClickHandler>(out var clickHandler))
+        {
+            if (clickHandler == null)
+            {
+                return;
+            }
+            clickHandler.OnPointerClick(new PointerEventData(EventSystem.current));
+        }
+    }
+
+    private void OnPointerOut(object sender, PointerEventArgs e)
+    {
+        if (e.target.TryGetComponent<IPointerExitHandler>(out var pointerExitHandler))
+        {
+
+
+            if (pointerExitHandler == null)
+            {
+                return;
+            }
+
+            pointerExitHandler.OnPointerExit(new PointerEventData(EventSystem.current));
+        }
+    }
+
+    private void OnPointerIn(object sender, PointerEventArgs e)
+    {
+        if (e.target.TryGetComponent<IPointerEnterHandler>(out var pointerEnterHandler))
+        {
+            if (pointerEnterHandler == null)
+            {
+                return;
+            }
+
+            pointerEnterHandler.OnPointerEnter(new PointerEventData(EventSystem.current));
+        }
+    }
+}
