a06f5024d4b8ff3aaa6c180e17163edb635c9a88
SarenDev <committer@saren.dev>
2025-01-20 16:05:25 +0100

    refactor(RecOptPanel): simplify script

--- Assets/Scripts/HomegrownScripts/MenuContents/RecOptPanel.cs
+++ Assets/Scripts/HomegrownScripts/MenuContents/RecOptPanel.cs
@@ -3,59 +3,63 @@ using UnityEngine.UI;
 using System;
 using System.Collections;
 using System.IO;
-using System.Text.RegularExpressions;
 using SimpleFileBrowser;
 
 public class RecOptPanel : MonoBehaviour
 {
     public Button[] buttons;
-    private TMPro.TextMeshProUGUI pathtext;
+    public TMPro.TextMeshProUGUI pathtext;
+    public BVHRecorder recorder;
     private string pathC;
     private int framerate;
-    private BVHRecorder recorder;
-    private Regex buttonMatch = new Regex("[0-9]+");
 
     //Loads the recorder parameters saved in Player Preferences, or sets the defaults
-    void Start(){
-        recorder = GameObject.Find("ViRe_Character").GetComponent<BVHRecorder>();
-        pathtext = GameObject.Find("PathField").GetComponentInChildren<TMPro.TextMeshProUGUI>();
-        pathC = PlayerPrefs.GetString("RecPath", System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+    void Start()
+    {
+        pathC = PlayerPrefs.GetString("RecPath", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
         framerate = PlayerPrefs.GetInt("RecFPS", 60);
         updateUI();
     }
 
-    public void changeFramerate(GameObject button){
-        framerate = Int32.Parse(buttonMatch.Match(button.name).Captures[0].ToString());
-        
+    public void changeFramerate(GameObject button)
+    {
+        framerate = int.Parse(button.name);
+
         writeSettings();
     }
-    
-    public void fileBrowser(){
+
+    public void fileBrowser()
+    {
         pathtext.text = "Check PC";
         StartCoroutine(runFileBrowser());
     }
-    
-    IEnumerator runFileBrowser(){
-        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Folders,false,pathC,null,"Select recording folder","Select");
-        if (FileBrowser.Success){pathC = FileBrowser.Result[0];}
+
+    IEnumerator runFileBrowser()
+    {
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Folders, false, pathC, null, "Select recording folder", "Select");
+        if (FileBrowser.Success) { pathC = FileBrowser.Result[0]; }
         writeSettings();
     }
 
-    private void updateUI(){
+    private void updateUI()
+    {
         pathtext.text = pathC;
 
-        foreach (Button button in buttons){
-            if (button.name == framerate+"FPS"){button.interactable = false;}
-            else{button.interactable = true;}
+        foreach (Button button in buttons)
+        {
+            if (button.name == framerate + "FPS") { button.interactable = false; }
+            else { button.interactable = true; }
         }
     }
 
     //Writes the settings to both Player Preferences and the Recorder, as well as output them to their fields
-    private void writeSettings(){
-        if (!Directory.Exists(pathC)){
-            pathC = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    private void writeSettings()
+    {
+        if (!Directory.Exists(pathC))
+        {
+            pathC = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
-        
+
         updateUI();
 
         recorder.directory = pathC;
